5160029d296d38bc8a78485de02befd47a0ac6ce
lmschmid <liam.schmid1212@gmail.com>
2019-04-15 23:43:43 -0700

    Changed tabsize to 4 spaces

--- logic/sessionStorage.js
+++ logic/sessionStorage.js
@@ -10,71 +10,72 @@ var gettingStoredStatsLocal = browser.storage.local.get();
 var gettingStoredStatsSync = browser.storage.sync.get();
 
 function setActiveListView(urls) {
-  gettingStoredStatsLocal.then(results => {
-    results["listview"] = {};
-    results["listview"] = Array.from(urls);
-    browser.storage.local.set(results);
-  });
+    gettingStoredStatsLocal.then(results => {
+        results["listview"] = {};
+        results["listview"] = Array.from(urls);
+        browser.storage.local.set(results);
+    });
 }
 
 function getActiveListView() {
-  return gettingStoredStatsLocal.then(results => {
-    return results["listview"];
-  });
+    return gettingStoredStatsLocal.then(results => {
+        return results["listview"];
+    });
 }
 
 function deleteSessionFromStorage(sessionName) {
-  gettingStoredStatsLocal.then(results => {
-    delete results.sessions[sessionName];
-    console.log("deleting storage");
-    console.log(results);
-    browser.storage.local.set(results);
-  });
+    gettingStoredStatsLocal.then(results => {
+        delete results.sessions[sessionName];
+        console.log("deleting storage");
+        console.log(results);
+        browser.storage.local.set(results);
+    });
 }
 
 function addSessionToStorage(urls, sessionName) {
-  // Load existent stats with the storage API.
-  console.log("in addToStorage, urls: "+urls)
-  return gettingStoredStatsLocal.then(results => {
-      // Initialize the saved stats if not yet initialized.
-      if (!("sessions" in results)) {
-        results = {
-          sessions : {}
-        };
-      }
-      
-      results["sessions"][sessionName] = urls;
-      console.log(results);
+    // Load existent stats with the storage API.
+    console.log("in addToStorage, urls: "+urls)
+    return gettingStoredStatsLocal.then(results => {
+        // Initialize the saved stats if not yet initialized.
+        if (!("sessions" in results)) {
+            results = {
+            sessions : {}
+            };
+        }
+        
+        results["sessions"][sessionName] = urls;
+        console.log(results);
 
-      // Persist the updated stats.
-      browser.storage.local.set(results) 
-  }).then(() => {return urls;});
+        // Persist the updated stats.
+        browser.storage.local.set(results) 
+    }).then(() => {return urls;});
 }
 
 function getSavedSessions() {
-  return gettingStoredStatsLocal.then(results => {
-      if (!("sessions" in results)) {
-        console.log("no sessions stored")
-        return {};
-      }
-      return results["sessions"];
+    return gettingStoredStatsLocal.then(results => {
+        console.log(results)
+        if (!("sessions" in results)) {
+            console.log("no sessions stored")
+            return {};
+        }
+        return results["sessions"];
     });
 }
 
 function clearSessions() {
-  let removeSessionsLocal =  browser.storage.local.remove("sessions");
-  let removeSessionsSync =  browser.storage.sync.remove("sessions");
+    let removeSessionsLocal =  browser.storage.local.remove("sessions");
+    let removeSessionsSync =  browser.storage.sync.remove("sessions");
 
-  removeSessionsLocal.then(onSessionsCleared, onSessionsClearedFail);
-  removeSessionsSync.then(onSessionsCleared, onSessionsClearedFail);
+    removeSessionsLocal.then(onSessionsCleared, onSessionsClearedFail);
+    removeSessionsSync.then(onSessionsCleared, onSessionsClearedFail);
 }
 
 function onSessionsCleared() {
-  console.log("sessions cleared");
-  let sessionsList = document.getElementById('sessions-list');
-  sessionsList.textContent = '';
+    console.log("sessions cleared");
+    let sessionsList = document.getElementById('sessions-list');
+    sessionsList.textContent = '';
 }
 
 function onSessionsClearedFail(e) {
-  console.log(e);
+    console.log(e);
 }
