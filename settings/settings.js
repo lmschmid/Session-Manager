c82353df4f7a69140f7399697859a89286e096ea
lmschmid <liam.schmid1212@gmail.com>
2019-04-24 17:18:51 -0700

    Progress on settings page

--- settings/settings.js
+++ settings/settings.js
@@ -1,8 +1,15 @@
+import { clearSessions, readFromLocalFile } from "../logic/sessionStorage.js";
 
-
-let restoreSessionInput = document.getElementById('restore-sessions');
+let restoreSessionInput = document.getElementById('json-button');
 restoreSessionInput.addEventListener("change", handleFiles, false);
 function handleFiles() {
     const fileList = this.files; /* now you can work with the file list */
     console.log(fileList);
 }
+
+
+document.addEventListener("click", async (e) => {
+    if (e.target.id == "clear-all-button") {
+        clearSessions();
+    }
+});
