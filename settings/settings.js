8e758a26daf4f8ffef59644a9f44978887cd9b6a
lmschmid <liam.schmid1212@gmail.com>
2019-04-24 20:05:19 -0700

    Added functional settings toggles

--- settings/settings.js
+++ settings/settings.js
@@ -1,4 +1,26 @@
 import { clearSessions, readFromLocalFile } from "../logic/sessionStorage.js";
+import { setShouldTabsLoad, setShouldRestoreWindow, shouldTabsLoad,
+         shouldRestoreWindow } from "../logic/settingsStorage.js";
+
+
+var shouldLoad, shouldRestore;
+
+function populateToggleSettings() {
+    let toggleWindowRestore = document.getElementById("window-settings");
+    let toggleLazyTabs = document.getElementById("lazy-tabs");
+
+    shouldTabsLoad().then((val) => {shouldLoad = val;}).then(() => {
+        shouldRestoreWindow().then((val) => {shouldRestore = val;}).then(() => {
+            console.log(shouldLoad);
+            console.log(shouldRestore);
+            if (!shouldLoad) {
+                toggleLazyTabs.checked = true;
+            } else if (shouldRestore) {
+                toggleWindowRestore.checked = true;
+            }
+        });
+    });
+}
 
 let restoreSessionInput = document.getElementById('json-button');
 restoreSessionInput.addEventListener("change", handleFiles, false);
@@ -8,8 +30,15 @@ function handleFiles() {
 }
 
 
+document.addEventListener("DOMContentLoaded", populateToggleSettings);
 document.addEventListener("click", async (e) => {
     if (e.target.id == "clear-all-button") {
         clearSessions();
+    }
+    else if (e.target.id == "window-settings") {
+        setShouldRestoreWindow(!shouldRestore);
+    }
+    else if (e.target.id == "lazy-tabs") {
+        setShouldTabsLoad(!shouldLazy);
     }
 });
