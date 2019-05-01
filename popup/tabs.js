a0f448705ccf80417ddce1b0d9706f6d58f4beb0
lmschmid <liam.schmid1212@gmail.com>
2019-04-30 23:46:55 -0700

    Slightly improved search bar animation

--- popup/tabs.js
+++ popup/tabs.js
@@ -375,7 +375,7 @@ document.addEventListener("click", async (e) => {
         const searchBar = document.getElementById('search-bar');
         if (searchBar.style.width === '164px') {
             searchBar.style.width = '0px';
-            searchBar.style.backgroundColor = 'rgb(202, 202, 202)';
+            setTimeout(function() {searchBar.style.backgroundColor = 'rgb(202, 202, 202)';}, 400);
         } else {
             searchBar.style.width = '164px';
             searchBar.style.backgroundColor = 'rgb(235, 235, 235)';
